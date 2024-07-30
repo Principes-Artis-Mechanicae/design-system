8ccbbb8c16319e4648758cc9291311a42ef0ad17
toothlessdev <toothless042@gmail.com>
2024-07-31 02:31:19 +0900

    migrate: design system 패키징 및 마이그레이션

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,34 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
 
-const config: StorybookConfig = {
-    stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-    addons: ["@storybook/addon-onboarding", "@storybook/addon-links", "@storybook/addon-essentials", "@chromatic-com/storybook", "@storybook/addon-interactions"],
+const config = {
+    stories: [
+        "../stories/**/*.mdx",
+        "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+        "../stories/**/*.story.@(js|jsx|mjs|ts|tsx)",
+    ],
+
+    addons: [
+        "@storybook/addon-onboarding",
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@chromatic-com/storybook",
+        "@storybook/addon-interactions",
+    ],
+
     framework: {
         name: "@storybook/react-vite",
         options: {},
+    },
+
+    docs: {},
+
+    webpackFinal: async (config) => {
+        config.resolve.alias = {
+            ...config.resolve?.alias,
+            "@": path.resolve(__dirname, "../packages"),
+        };
+        return config;
     },
 };
 export default config;
---
+++ stories/Pagination.story.ts
@@ -0,0 +1,20 @@
+import { Pagination } from "../packages/components/navigation/Pagination";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+    title: "Navigation/Pagination",
+    component: Pagination,
+} satisfies Meta<typeof Pagination>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        totalItems: 125,
+        itemCountPerPage: 5,
+        pageCount: 10,
+        currentPage: 1,
+        basePath: "/example",
+    },
+};
